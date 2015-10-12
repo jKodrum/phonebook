006a5da47113960d76667950cc5b6e06120992b3
jKodrum <jkodrum@gmail.com>
2015-10-12 22:42:19 +0800

    not allocate memory to "info" in an entry
    The purpose of separating several members of "entry" to another
    structure "info" is to decrease cache-miss while searching.
    In append() fucntion, allocate memory to "info" of each phonebook
    entry cannot achieve this purpose.
    
    But not allocating memory to "info" seems to ignore this member.
    
    Therefore, I assume the "info" member isn't used for now.
    append() function doesn't append detail infomation but lastName.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -17,7 +17,6 @@ entry *append(char lastName[], entry *e)
 {
     e->pNext = (entry *) malloc(sizeof(entry));
     e = e->pNext;
-    e->info = (content *)malloc(sizeof(content));
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
 
